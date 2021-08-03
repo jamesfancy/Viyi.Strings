1fb7c4281c8bd96c72b7fb5923fda341d0976b61
James Fan <jamesfancy@126.com>
2021-08-03 23:39:48 +0800

    调整 EncodeHex 重载，去掉调用冲突

--- Viyi.Strings/Codec/Hex/HexExtensions.cs
+++ Viyi.Strings/Codec/Hex/HexExtensions.cs
@@ -12,19 +12,21 @@ namespace Viyi.Strings.Codec.Hex
             return new HexCodec().Decode(hex);
         }
 
-        public static string EncodeHex(this byte[] bytes, CodecOptions? options = null)
+        public static string EncodeHex(this byte[] bytes, CodecOptions? options)
         {
             return bytes.IsEmpty()
                 ? string.Empty
                 : new HexCodec().Encode(bytes, options);
         }
 
+        public static string EncodeHex(this byte[] bytes) => EncodeHex(bytes, null);
+
         /// <summary>
-        /// 
+        /// 把二进制数据转换成十六进制文本，可以指定字母大小写，以及折行位置。
         /// </summary>
         /// <param name="bytes"></param>
-        /// <param name="upperCase">是否以大写字母输出。默认为 false。</param>
-        /// <param name="lineWidth"></param>
+        /// <param name="upperCase">是否以大写字母输出。默认为 false，即小写字母输出。</param>
+        /// <param name="lineWidth">默认的换行符是 LF，如果需要使用其他换行符，使用自定义的 CodecOptions。</param>
         /// <returns></returns>
         public static string EncodeHex(this byte[] bytes, bool upperCase, int lineWidth)
         {
@@ -38,13 +40,16 @@ namespace Viyi.Strings.Codec.Hex
         }
 
         /// <summary>
-        /// 
+        /// 把二进制数据转换成十六进制文本，可以指定字母大小写，以及是否折行。
         /// </summary>
         /// <param name="bytes"></param>
-        /// <param name="upperCase">是否以大写字母输出。默认为 false。</param>
-        /// <param name="lineBreak">是否以默认宽度（76 字符）换行，默认为 false。</param>
+        /// <param name="upperCase">是否以大写字母输出。默认为 false，即小写字母。</param>
+        /// <param name="lineBreak">
+        /// 是否以默认宽度（76 字符）换行，默认为 false。
+        /// 默认换行符为 LF，如果需要使用其他换行符，使用自定义的 CodecOptions。
+        /// </param>
         /// <returns></returns>
-        public static string EncodeHex(this byte[] bytes, bool upperCase = false, bool lineBreak = false)
+        public static string EncodeHex(this byte[] bytes, bool upperCase, bool lineBreak = false)
         {
             return EncodeHex(
                 bytes,
