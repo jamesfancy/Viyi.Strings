351b4055e990fbb3b852eba19628c570468381d0
James Fan <jamesfancy@126.com>
2021-08-29 09:38:11 +0800

    添加 EmptyAs 和 SpacesAs 扩展方法

--- Viyi.Strings/Extensions/StringExtensions.cs
+++ Viyi.Strings/Extensions/StringExtensions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace Viyi.Strings.Extensions {
@@ -8,6 +9,28 @@ namespace Viyi.Strings.Extensions {
 
         public static string Repeat(this char ch, int count) {
             return new string(ch, count);
+        }
+
+        /// <summary>
+        /// 扩展方法。如果当前字符串是 null 或者空字符串 ("")，则返回指定值；
+        /// 否则返回当前字符串。
+        /// 如果需要保留 null，应该使用可空链运算符调用，如 `str?.EmptyAs()`。
+        /// </summary>
+        /// <param name="value">指定用于代替空串的值</param>
+        [return: NotNullIfNotNull("value")]
+        public static string? EmptyAs(this string? str, string? value) {
+            return string.IsNullOrEmpty(str) ? value : str;
+        }
+
+        /// <summary>
+        /// 扩展方法。如果当前字符串是 null、空字符串 ("") 或仅含空白字符，则返回指定值；
+        /// 否则返回当前字符串。
+        /// 如果需要保留 null，应该使用可空链运算符调用，如 `str?.SpacesAs()`。
+        /// </summary>
+        /// <param name="value">指定用于代替空白字符串的值</param>
+        [return: NotNullIfNotNull("value")]
+        public static string? SpacesAs(this string? str, string? value) {
+            return string.IsNullOrWhiteSpace(str) ? value : str;
         }
     }
 }
