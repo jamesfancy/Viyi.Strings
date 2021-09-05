b586d92fa1d3a210ed05eaeadefc624dc374c571
James Fan <jamesfancy@126.com>
2021-09-05 22:01:12 +0800

    为 CaseConvert 添加注册新转换器的接口

---
+++ Viyi.Strings/CaseConverters/LambdaCaseConverter.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Viyi.Strings.CaseConverters {
+    public sealed class LambdaCaseConverter : ICaseConverter {
+        readonly Func<string?, string?> convert;
+
+        public LambdaCaseConverter(Func<string?, string?> convert) {
+            this.convert = convert ?? throw new ArgumentNullException(nameof(convert));
+        }
+
+        [return: NotNullIfNotNull("value")]
+        public string? Convert(string? value) => convert(value);
+    }
+}
