17f9f32a92c743fad585c31b25307868cb290268
边城 (James Fan) <jamesfancy@126.com>
2022-06-02 17:13:18 +0800

    Hex 正名为 Base16，同时保留 Hex
    comment #I4JWNC

--- Viyi.Strings/Codec/Base16/Base16Encoder.cs
+++ Viyi.Strings/Codec/Base16/Base16Encoder.cs
@@ -3,8 +3,8 @@ using Viyi.Strings.Codec.Abstract;
 using Viyi.Strings.Codec.Io;
 using Viyi.Strings.Codec.Options;
 
-namespace Viyi.Strings.Codec {
-    class HexEncoder : TextEncoder {
+namespace Viyi.Strings.Codec.Base16 {
+    class Base16Encoder : TextEncoder {
         static readonly char[] UpperHexChars = {
             '0', '1', '2', '3', '4', '5', '6', '7',
             '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
@@ -17,7 +17,7 @@ namespace Viyi.Strings.Codec {
 
         readonly char[] hexChars;
 
-        public HexEncoder(CodecOptions options) : base(options) {
+        public Base16Encoder(CodecOptions options) : base(options) {
             hexChars = options.UpperCase ? UpperHexChars : LowerHexChars;
         }
 
@@ -26,8 +26,8 @@ namespace Viyi.Strings.Codec {
             int b;
             char[] chars = new char[2];
             while ((b = bs.ReadByte()) >= 0) {
-                chars[0] = (hexChars[b >> 4]);
-                chars[1] = (hexChars[b & 0x0f]);
+                chars[0] = hexChars[b >> 4];
+                chars[1] = hexChars[b & 0x0f];
                 writer.Write(chars);
             }
         }
--- Viyi.Strings/Codec/Hex/HexCodec.cs
+++ Viyi.Strings/Codec/Hex/HexCodec.cs
@@ -1,14 +1,17 @@
-using Viyi.Strings.Codec.Abstract;
+using Viyi.Strings.Codec.Base16;
 using Viyi.Strings.Codec.Options;
 
 namespace Viyi.Strings.Codec {
-    public class HexCodec : CodecBase {
-        public override ITextDecoder CreateDecoder(CodecOptions? options = null) {
-            return new HexDecoder(options ?? CodecOptions.CreateDefault());
-        }
+    /// <summary>十六进制编解码器</summary>
+    /// <remarks>和 Base16 功能一样，应该使用 Baes16 代替</remarks>
+    public class HexCodec : Base16Codec { }
 
-        public override ITextEncoder CreateEncoder(CodecOptions? options = null) {
-            return new HexEncoder(options ?? CodecOptions.CreateDefault());
+    internal class HexDecoder : Base16Decoder {
+        public HexDecoder(CodecOptions options) : base(options) { }
+    }
+
+    class HexEncoder : Base16Encoder {
+        public HexEncoder(CodecOptions options) : base(options) {
         }
     }
 }
