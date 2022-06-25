33b9e882f70e14c1afc0fd8d5857832642815de6
边城 (James Fan) <jamesfancy@126.com>
2022-06-25 14:43:48 +0800

    完善 CodecOptions 的创建逻辑并提供 CreatePure 创建不受 DefaultCreator 影响的配置

--- Viyi.Strings.Test/Codec/Options/CodecOptionsTests.cs
+++ Viyi.Strings.Test/Codec/Options/CodecOptionsTests.cs
@@ -1,4 +1,3 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Viyi.Strings.Codec.Options;
 
 namespace Viyi.Strings.Test.Codec.Options {
@@ -47,6 +46,47 @@ namespace Viyi.Strings.Test.Codec.Options {
         public void TestNonNegativeLineWidth() {
             var opts = CodecOptions.Create().SetLineWidth(-1).Build();
             Assert.AreEqual(0, opts.LineWidth);
+        }
+
+        [TestMethod]
+        public void TestDefaultCreator() {
+            Assert.IsNull(CodecOptions.DefaultCreator);
+
+            CodecOptions.DefaultCreator = () => CodecOptions.Create().Build();
+            var opt1 = CodecOptions.DefaultCreator();
+            var opt2 = CodecOptions.DefaultCreator();
+            Assert.IsNotNull(opt1);
+            Assert.IsNotNull(opt2);
+            Assert.AreEqual(opt1, opt2);
+            Assert.AreNotSame(opt1, opt2);
+
+            // 恢复为 null
+            CodecOptions.DefaultCreator = null;
+        }
+
+        [TestMethod]
+        public void TestOptionsBuilder() {
+            Assert.AreEqual(CodecOptions.Default, CodecOptions.CreatePure().Build());
+
+            var opt1 = CodecOptions.Create().Build();
+            Assert.AreEqual(CodecOptions.Default, opt1);
+            Assert.AreNotSame(CodecOptions.Default, opt1);
+
+            var prototype = CodecOptions.Create()
+                .SetLineEnding(LineEndings.Cr)
+                .SetLineWidth(77)
+                .UseUpperCase()
+                .Build();
+            Assert.AreNotEqual(CodecOptions.Default, prototype);
+            Assert.AreEqual(prototype, CodecOptions.Create(prototype).Build());
+            Assert.AreNotSame(prototype, CodecOptions.Create(prototype).Build());
+
+            CodecOptions.DefaultCreator = () => CodecOptions.Create(prototype).Build();
+            Assert.AreEqual(CodecOptions.Default, CodecOptions.CreatePure().Build());
+            Assert.AreNotEqual(CodecOptions.Default, CodecOptions.Create().Build());
+            Assert.AreNotEqual(prototype, CodecOptions.CreatePure().Build());
+            Assert.AreEqual(prototype, CodecOptions.Create().Build());
+            Assert.AreNotSame(prototype, CodecOptions.Create().Build());
         }
     }
 }
