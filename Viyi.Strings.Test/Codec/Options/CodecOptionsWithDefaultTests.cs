85ce8260dc641b7c89a0be4659dc1aadd912fdaf
边城 (James Fan) <jamesfancy@126.com>
2026-03-03 14:56:45 +0800

    fix: 并发测试引起静态成员值不确定的问题

--- Viyi.Strings.Test/Codec/Options/CodecOptionsWithDefaultTests.cs
+++ Viyi.Strings.Test/Codec/Options/CodecOptionsWithDefaultTests.cs
@@ -2,8 +2,13 @@ using Viyi.Strings.Codec.Options;
 
 namespace Viyi.Strings.Test.Codec.Options;
 
-[TestClass]
+[TestClass, DoNotParallelize]
 public class CodecOptionsWithDefaultTests {
+    [TestCleanup]
+    public void Cleanup() {
+        CodecOptions.SetDefaultCreator(null);
+    }
+
     [TestMethod]
     public void TestSetDefaultCreator() {
         CodecOptions.SetDefaultCreator(_ => { });
